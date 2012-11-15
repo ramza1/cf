f619c9ca5966eb0800969f2b362bb43d198b78cf
maikel <maikel@ubuntu.ubuntu-domain>
2012-11-15 22:47:00 +0100

    deployement configs

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :application, "cf"
 set :user, "deployer"
 set :deploy_to, "/home/#{user}/apps/#{application}"
 set :deploy_via, :remote_cache
-set :use_sudo, false
+set :use_sudo, true
 
 set :scm, "git"
 set :repository, "git@github.com:ramza1/#{application}.git"
