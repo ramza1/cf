a4c33ff8ed1f1cb558b034585f79b71eba1dd3df
maikel <maikel@ubuntu.ubuntu-domain>
2012-11-15 17:50:20 +0100

    deployement configs

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,10 @@
 require "bundler/capistrano"
 
-server "208.68.37.172", :web, :app, :db, primary: true
+server "rzaartz@208.68.37.172", :web, :app, :db, primary: true
 
 set :application, "cf"
 set :user, "rzaartz"
-set :deploy_to, "/home/#{user}/apps/#{application}"
+set :deploy_to, "/home/#{user}/#{application}"
 set :deploy_via, :remote_cache
 set :use_sudo, false
 
