c36b03599f02988d268aa598590cef8a9639b458
maikel <maikel@ubuntu.ubuntu-domain>
2012-11-16 18:17:53 +0100

    masiv updates

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Cf::Application.config.secret_token = '849cb67562a70e78f8c6ec4d3f0a1af25bf990e48a83a9d473843bb0b07e9f6abc319fb75bef21740c5c905840574b2ec05c3b90071762f21c9a5b8b3f9f55a1'
+Cf::Application.config.secret_token = 'de1b517b1fabcb8c92b607914fd747baa4bcf5bcaf3e9beffafb35aea815a3281a79cc8fd218e1951a5d8bd10985f9ddb5c6bd2d93a6bc6c4bc3ac9673c24c6b'
