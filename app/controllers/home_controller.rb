a44a990cfc5d7268b5c4303e83f33f9cb5f3aaeb
maikel <maikel@ubuntu.ubuntu-domain>
2012-11-21 13:03:46 +0100

    adjusted home details

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,13 +2,13 @@ class HomeController < ApplicationController
   def index
     @blogs = Blog.published.recent.limit(3)
     @fashion_juries = FashionJury.published.recent.limit(3)
-    @men_fashion = MenFashion.published.recent.last
+    @men_fashion = MenFashion.published.recent.first
     @functional_fashions = FunctionalFashion.published.recent.limit(2)
     @collections =  Runway::Collection.published.recent.limit(4)
-    @trend_spotting = TrendSpotting.published.recent.includes(:photos).last
+    @trend_spotting = TrendSpotting.published.recent.includes(:photos).first
     @photos = @trend_spotting.photos.limit(4)  if @trend_spotting
     @videos = Video.published.recent.limit(7)
-    @redcarpet = Redcarpet.published.recent.includes(:photos).last
+    @redcarpet = Redcarpet.published.recent.includes(:photos).first
     if @redcarpet
     @redcarpet_photos = @redcarpet.photos.limit(10)
     if @redcarpet_photos.size >= 10
